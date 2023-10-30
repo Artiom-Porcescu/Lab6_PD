da97e57c68026121f663a8c7348f9293a1a76031
Artiom Porcescu <43860271+Artiom-Porcescu@users.noreply.github.com>
2023-10-30 11:16:19 +0200

    added functionality to check if object exists i.e. is not None

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ class TestCurrencyData(unittest.TestCase):
         result = get_data_for_date(test_date, test_file)
         self.assertAlmostEqual(result, 96.3344)
 
+    # Тестирование возвращения None при отсутствии данных о курсе валюты на заданную дату
+    def test_get_data_for_date_invalid(self):
+        test_file = 'dataset.csv'
+        test_date = datetime.strptime('2023/11/01', '%Y/%m/%d')
+        result = get_data_for_date(test_date, test_file)
+        self.assertIsNone(result)
+
 if __name__ == '__main__':
     unittest.main()
